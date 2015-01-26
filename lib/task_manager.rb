f921621b81199317e563f806f280c14d515b1e77
Rachel Warbelow <rachelwarbelow@gmail.com>
2015-01-25 18:40:20 -0800

    initial commit

---
+++ lib/task_manager.rb
@@ -0,0 +1,48 @@
+require 'yaml/store'
+
+class TaskManager
+  def self.create(task)
+    database.transaction do
+      database['tasks'] ||= []
+      database['total'] ||= 0
+      database['total'] += 1
+      database['tasks'] << { "id" => database['total'], "title" => task[:title], "description" => task[:description] }
+    end
+  end
+
+  def self.all
+    raw_tasks.map { |data| Task.new(data) }
+  end
+
+  def self.raw_tasks
+    database.transaction do
+      database['tasks'] || []
+    end
+  end
+
+  def self.raw_task(id)
+    raw_tasks.find { |task| task["id"] == id }
+  end
+
+  def self.find(id)
+    Task.new(raw_task(id))
+  end
+
+  def self.update(id, task)
+    database.transaction do 
+      target = database['tasks'].find { |data| data["id"] == id }
+      target["title"] = task[:title]
+      target["description"] = task[:description]
+    end
+  end
+
+  def self.delete(id)
+    database.transaction do
+      database['tasks'].delete_if { |task| task["id"] == id }
+    end
+  end
+
+  def self.database
+    @database ||= YAML::Store.new("db/task_manager")
+  end
+end
